4ce7529884cd2a02a1112ab575c68b8059133217
Vadik <nikolaevvadim92@gmail.com>
2024-02-18 19:09:25 +0200

    Task1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-bbc
+a=int(input('enter 1 digit '))
+b=int(input('enter 1 second digit '))
+c=int(input('enter third digit '))
+num=input('enter 1-3. 1-max,2-min,3-mean ')
+if num=='1':
+    if a>b and a>c:
+        print(a)
+    elif b>a and b>c:
+        print(b)
+    elif c>a and c>b:
+        print(c)
+if num=='2':
+    if a<b and a<c:
+        print(a)
+    elif b<a and b<c:
+        print (b)
+    elif c<a and c<b:
+        print(c)
+if num=='3':
+    print((a+b+c)/3)
+
