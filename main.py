08b5a5f8af880befb86677ac782644e90e127983
Vadik <nikolaevvadim92@gmail.com>
2024-02-18 19:10:33 +0200

    reformat

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-a=int(input('enter 1 digit '))
-b=int(input('enter 1 second digit '))
-c=int(input('enter third digit '))
-num=input('enter 1-3. 1-max,2-min,3-mean ')
-if num=='1':
-    if a>b and a>c:
+a = int(input('enter 1 digit '))
+b = int(input('enter 1 second digit '))
+c = int(input('enter third digit '))
+num = input('enter 1-3. 1-max,2-min,3-mean ')
+if num == '1':
+    if a > b and a > c:
         print(a)
-    elif b>a and b>c:
+    elif b > a and b > c:
         print(b)
-    elif c>a and c>b:
+    elif c > a and c > b:
         print(c)
-if num=='2':
-    if a<b and a<c:
+if num == '2':
+    if a < b and a < c:
         print(a)
-    elif b<a and b<c:
-        print (b)
-    elif c<a and c<b:
+    elif b < a and b < c:
+        print(b)
+    elif c < a and c < b:
         print(c)
-if num=='3':
-    print((a+b+c)/3)
-
+if num == '3':
+    print((a + b + c) / 3)
